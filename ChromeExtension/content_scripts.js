4b278e59b02a90e326df0532b2f53c4a56a607a0
madisonmay13 <madison.may@students.olin.edu>
2013-03-19 11:16:14 -0400

    Initial commit

---
+++ ChromeExtension/content_scripts.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    console.log("External script is ready");
+})
---
+++ ChromeExtension/popup.js
@@ -0,0 +1,10 @@
+
+function ready() {
+  console.log("Ready");
+};
+
+
+//When the DOM is ready, do the followin
+document.addEventListener('DOMContentLoaded', function () {
+  ready();
+});
